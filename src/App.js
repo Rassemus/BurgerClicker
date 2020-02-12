c16545334b4ae1888effd9eb3b586bfed8798249
Rasmus Granholm <rasse91@gmail.com>
2020-02-12 20:23:10 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { Component, useState } from 'react';
+
+import './App.css';
+
+import burger from './burger.png';
+import iconburger from './icon-burger.png';
+import iconboy from './icon-boy.png';
+import iconcoupon from './icon-coupon.png';
+
+class Clicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0
+    };
+    this.mouseClicked = this.mouseClicked.bind(this);
+  }
+
+  mouseClicked() {
+    const clicks = this.state.clicks;
+    this.setState({
+      clicks: clicks + 1
+    });
+  }
+
+  render() {
+    return (
+      <div className='clicker'>
+        <div className='header'>
+          <h1>Burger Clicker</h1>
+        </div>
+        <div className='content content--justified'>
+          <Stats count={this.state.clicks} />
+          <Burger onClick={this.mouseClicked} />
+          <Booster boost={2.5} />
+        </div>
+        <Main claimableCoupons={5} />
+      </div>
+    );
+  }
+}
+
+function Stats(props) {
+  return (
+    <div className='stats'>
+      <div className='stats_title'>burgers</div>
+      <div className='stats_count'>{props.count}</div>
+    </div>
+  );
+}
+
+function Burger(props) {
+  const [pressed, setPressed] = useState(false);
+  const classValue = pressed
+    ? 'burger__img burger__img--pressed'
+    : 'burger__img';
+  return (
+    <div className='burger'>
+      <img
+        src={burger}
+        alt=''
+        className={classValue}
+        onClick={props.onClick}
+        onPointerDown={() => setPressed(true)}
+        onPointerUp={() => setPressed(false)}
+      />
+    </div>
+  );
+}
+
+function Booster(props) {
+  return <div className='booster'>{props.boost} burgers / click</div>;
+}
+
+function Main(props) {
+  return (
+    <div className='menu'>
+      <div>
+        <img src={iconburger} alt='game' />
+      </div>
+      <div>
+        <img src={iconcoupon} alt='coupon' />
+        <span className='badge'>{props.claimableCoupons}</span>
+      </div>
+      <div>
+        <img src={iconboy} alt='profile' />
+      </div>
+    </div>
+  );
+}
+
+export default Clicker;
